cf10b42f1079a3de47f88281b14f73cf595b1a50
Artur Świerc <artur@appjobs.com>
2021-05-02 18:29:53 +0200

    remove leavings

--- index.js
+++ index.js
@@ -4,13 +4,4 @@ const PORT = process.env.PORT || 5000;
 
 let app = express();
 
-app.use(express.static(path.join(__dirname, 'src/public')))
-    .set('views', path.join(__dirname, 'src/views'))
-    .set('view engine', 'ejs');
-
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/test', function (req, res) {
-    res.send('Hello World!');
-});
-
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
