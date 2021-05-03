56c9df940305e43902ee0d16df7414b9e5ef79f1
Artur Świerc <aswierc@gmail.com>
2021-05-03 12:21:32 +0200

    git ignore and setup database

--- bin/setupDatabase.js
+++ bin/setupDatabase.js
@@ -3,5 +3,5 @@ const sequelize = require('../src/infrastructure/database/sequelize')
 const Offer = require('../src/infrastructure/model/offer')
 
 sequelize.sync({
-    force: true
+    // force: true
 })
