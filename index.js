16121503690e585b2a9768c50a8a16e4ac5a805b
Artur Świerc <aswierc@gmail.com>
2021-05-08 22:49:18 +0200

    change offers requedt, cors, endpoint offers

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 const offerQuery = require('./src/application/query/offersQuery')
+const cors = require('cors')
+
+const dotenv = require('dotenv');
+dotenv.config()
 
 let app = express();
 
-app.get('/', function (req, res) {
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+}
+
+app.get('/', (req, res) => {
     res.send('Pong')
 })
 
-app.get('/offers', function (req, res) {
+app.get('/offers', cors(corsOptions), (req, res) => {
     offerQuery.getAll().then(entries => {
         res.json(entries)
     })
--- src/application/query/offersQuery.js
+++ src/application/query/offersQuery.js
@@ -2,15 +2,46 @@ const { QueryTypes } = require('sequelize');
 const sequelize = require('../../infrastructure/database/sequelize')
 
 async function getAll() {
+    const query = 'with offers_ordered as (' +
+        'select  ' +
+        'row_number() over(partition by "offerId" order by "createdAt" desc) as rowNo,' +
+        '"offerId", ' +
+        'name, ' +
+        'image, ' +
+        '"watchersCount",' +
+        '"visitsCount",' +
+        '"soldCount",' +
+        '"publicationDate", ' +
+        '"createdAt"' +
+        'from offers' +
+        ')' +
+        '' +
+        'select ' +
+        'o."offerId",' +
+        'o.name,' +
+        'ps.price, ' +
+        'ps.stock,' +
+        'o.image,' +
+        'o."watchersCount",' +
+        'o."visitsCount",' +
+        'o."soldCount",' +
+        'o."publicationDate" ' +
+        'from price_stocks ps ' +
+        'join  offers_ordered o on o."offerId" = ps."offerId" ' +
+        'and rowNo = 1' +
+        'group by ' +
+        'o."offerId",' +
+        'o.name,' +
+        'ps.price,' +
+        'ps.stock,' +
+        'o.image,' +
+        'o."watchersCount",' +
+        'o."visitsCount",' +
+        'o."soldCount",' +
+        'o."publicationDate"' +
+        'order by o."soldCount" desc'
 
-    const query = 'select ' +
-        'ps."offerId", o.name, ps.price, ps.stock, (ps.price*ps.stock) as sum_stock from price_stocks ps\n' +
-        '    join offers o on o."offerId" = ps."offerId" ' +
-        '    where ps.stock > 0 ' +
-        '    group by ps."offerId", ps.price, ps.stock, o.name ' +
-        '    order by ps."offerId"'
-
-    return await sequelize.query(query, {type: QueryTypes.SELECT})
+    return await sequelize.query(query, { type: QueryTypes.SELECT })
 }
 
 module.exports = {
